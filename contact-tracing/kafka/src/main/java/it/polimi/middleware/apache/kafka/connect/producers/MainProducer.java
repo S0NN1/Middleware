a25f4fb8e1f6b187ce973b2ce6f76f70b8261ce0
Luca Pirovano <luca.pirovano.98@gmail.com>
2021-04-18 21:12:30 +0200

    p5

--- contact-tracing/kafka/src/main/java/it/polimi/middleware/apache/kafka/connect/producers/MainProducer.java
+++ contact-tracing/kafka/src/main/java/it/polimi/middleware/apache/kafka/connect/producers/MainProducer.java
@@ -10,12 +10,13 @@ import java.util.Properties;
 public class MainProducer {
     public static void main(String[] args) {
         Properties props = new Properties();
-            props.put("bootstrap.servers","51.103.30.161:9092");
+        props.put("bootstrap.servers","51.103.30.161:9092");
         props.put("acks","all");
         props.put("key.serializer", StringSerializer.class.getName());
         props.put("value.serializer", StringSerializer.class.getName());
         Producer<String,String> producer = new KafkaProducer<String, String>(props);
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Itero");
             String key = Integer.toString(i);
             String value = "PIRO GAY" + key;
             producer.send(new ProducerRecord<String, String>("quickstart-events", key, value));
