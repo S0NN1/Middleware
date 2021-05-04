d7200e633c150adce91693d0d23965551682b81c
--DOT_GITCONFIG_FULL_NAME-- <--DOT_GITCONFIG_GIT_EMAIL-->
2021-05-04 23:12:57 +0200

    kafka: created streams, fixed tables publish on mqtt

--- contact-tracing/kafka/src/main/java/it/polimi/middleware/apache/kafka/connect/consumers/MainConsumer.java
+++ contact-tracing/kafka/src/main/java/it/polimi/middleware/apache/kafka/connect/consumers/MainConsumer.java
@@ -1,32 +1,25 @@
 package it.polimi.middleware.apache.kafka.connect.consumers;
 
+import io.confluent.ksql.api.client.BatchedQueryResult;
 import io.confluent.ksql.api.client.Client;
+import io.confluent.ksql.api.client.Row;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 
 import java.time.Duration;
-import java.util.Arrays;
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static it.polimi.middleware.apache.kafka.connect.utils.Utils.createKSQLClient;
 import static it.polimi.middleware.apache.kafka.connect.utils.Utils.setupConsumerProps;
 
 public class MainConsumer {
-    private static final String ksqlStream = "CREATE STREAM CONNECTIONS (" +
-            "EventType VARCHAR , " +
-            "ClientId VARCHAR, " +
-            "SenderClientId VARCHAR) " +
-            "WITH (" +
-            "KAFKA_TOPIC='mqtt-to-kafka-connections', " +
-            "VALUE_FORMAT='json');";
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = setupConsumerProps();
         KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
         consumer.subscribe(Arrays.asList("mqtt-to-kafka-connections"));
-        Client ksqlClient = createKSQLClient();
-        ksqlClient.executeStatement(ksqlStream).get();
         while (true) {
             ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
             for (ConsumerRecord<String, String> record : records) {
--- contact-tracing/kafka/src/main/java/it/polimi/middleware/apache/kafka/connect/producers/MainProducer.java
+++ contact-tracing/kafka/src/main/java/it/polimi/middleware/apache/kafka/connect/producers/MainProducer.java
@@ -1,42 +1,97 @@
 package it.polimi.middleware.apache.kafka.connect.producers;
 
-import io.confluent.ksql.api.client.Client;
-import io.confluent.ksql.api.client.Row;
-import io.confluent.ksql.api.client.StreamedQueryResult;
+import io.confluent.ksql.api.client.*;
 import it.polimi.middleware.apache.kafka.connect.utils.Utils;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.Properties;
+import java.util.*;
 import java.util.concurrent.ExecutionException;
 
 import static it.polimi.middleware.apache.kafka.connect.utils.Utils.createKSQLClient;
 
 public class MainProducer {
-    private static final String ksqlQuery = "SELECT * FROM CONNECTIONS EMIT CHANGES;";
+    private static final Logger logger = LoggerFactory.getLogger("Logger");
+    private static final String connectionStream = "CREATE STREAM CONNECTIONS (" +
+            "EventType VARCHAR , " +
+            "ClientId VARCHAR, " +
+            "SenderClientId VARCHAR, " +
+            "Identifier VARCHAR) " +
+            "WITH (" +
+            "KAFKA_TOPIC='mqtt-to-kafka-connections', " +
+            "VALUE_FORMAT='json');";
+    private static final String alertStream = "CREATE STREAM ALERTS (" +
+            "EventType VARCHAR , " +
+            "ClientId VARCHAR) " +
+            "WITH (" +
+            "KAFKA_TOPIC='mqtt-to-kafka-alerts', " +
+            "VALUE_FORMAT='json');";
+    private static final String connectionTableQuery= "CREATE TABLE CONNECTION_TABLE AS\n" +
+            "    SELECT IDENTIFIER,\n" +
+            "    LATEST_BY_OFFSET(CLIENTID) AS CLIENTID,\n" +
+            "    LATEST_BY_OFFSET(SENDERCLIENTID) AS SENDERCLIENTID\n" +
+            "    FROM  CONNECTIONS \n" +
+            "    GROUP BY IDENTIFIER\n" +
+            "    EMIT CHANGES;";
+    private static final String ksqlQuery = "SELECT * FROM ALERTS EMIT CHANGES;";
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         Properties props = Utils.setupProducerProps();
         Producer<String, String> producer = new KafkaProducer<String, String>(props);
         Client ksqlClient = createKSQLClient();
-        StreamedQueryResult streamedQueryResult = ksqlClient.streamQuery(ksqlQuery).get();
-
-        while(true) {
-            // Block until a new row is available
-            Row row = streamedQueryResult.poll();
-            if (row != null) {
-                System.out.println(row.values());
-            } else {
-                System.out.println("Query has ended.");
-                break;
+        List<StreamInfo> streams = ksqlClient.listStreams().get();
+        boolean alertStreamFound = false;
+        boolean connectionStreamFound = false;
+        for(StreamInfo stream : streams) {
+            if(stream.getName().equals("ALERTS")){
+                alertStreamFound = true;
+            }
+            else if(stream.getName().equals("CONNECTIONS")) {
+                connectionStreamFound = true;
             }
         }
-//        for (int i = 0; i < 10; i++) {
-//            String key = Integer.toString(i);
-//            String value = "PIRO GAY" + key;
-//            producer.send(new ProducerRecord<String, String>("kafka-to-mqtt-alert", key, value));
-//        }
-//        producer.close();
+        if(!alertStreamFound){
+            //Create Kafka Alert Stream
+            ksqlClient.executeStatement(alertStream).get();
+        }
+        if (!connectionStreamFound){
+            //Create Kafka Connection Stream
+            ksqlClient.executeStatement(connectionStream).get();
+        }
+        boolean connectionTableFound = false;
+        List<TableInfo> tables = ksqlClient.listTables().get();
+        for (TableInfo table : tables){
+            if (table.getName().equals("CONNECTION_TABLE")){
+                connectionTableFound = true;
+            }
+        }
+        if (!connectionTableFound){
+            Map<String, Object> properties = Collections.singletonMap("auto.offset.reset", "earliest");
+            ksqlClient.executeStatement(connectionTableQuery, properties).get();
+        }
+        StreamedQueryResult streamedQueryResult = ksqlClient.streamQuery(ksqlQuery).get();
+        while(true) {
+            Row row = streamedQueryResult.poll();
+            String clientId = row.values().getString(1);
+            String pullQuery = "SELECT * FROM CONNECTION_TABLE WHERE CLIENTID='"+ clientId + "';";
+            Map<String, Object> properties = new HashMap<>();
+            properties.put("auto.offset.reset", "latest");
+            properties.put("ksql.query.pull.table.scan.enabled", true);
+            List<Row> queryResult1 = ksqlClient.executeQuery(pullQuery, properties).get();
+            logger.info("Received an alert from client " + clientId + ". Creating a mqtt alert message...");
+            for (Row result : queryResult1){
+               Random random = new Random();
+               String key = Integer.toString(random.nextInt());
+               String value = "{" +
+                       "\"AlertDest\":\""+ result.getValue(3) + "\", " +
+                       "\"AlertSource\":\"" + clientId + "\"" +
+                       "}";
+               producer.send(new ProducerRecord<String, String>("kafka-to-mqtt-alerts", key, value));
+            }
+            logger.info("Alerts sent to clients who has been in contact with " + clientId);
+        }
     }
 }
