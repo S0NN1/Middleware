3fe104439f268473b98d256971cfc0f8972bd51c
--DOT_GITCONFIG_FULL_NAME-- <--DOT_GITCONFIG_GIT_EMAIL-->
2021-05-05 21:32:51 +0200

    fix query breaking whole alert

--- contact-tracing/kafka/src/main/java/it/polimi/middleware/kafka/utils/Utils.java
+++ contact-tracing/kafka/src/main/java/it/polimi/middleware/kafka/utils/Utils.java
@@ -1,4 +1,4 @@
-package it.polimi.middleware.apache.kafka.connect.utils;
+package it.polimi.middleware.kafka.utils;
 
 import io.confluent.ksql.api.client.Client;
 import io.confluent.ksql.api.client.ClientOptions;
@@ -11,22 +11,13 @@ public class Utils {
     private static final String KAFKA_BROKER_IP = "51.103.25.211";
     private static final String KAFKA_BROKER_PORT = "9092";
     private static final int KAFKA_KSQL_PORT = 8088;
-
-    public static Properties setupConsumerProps() {
-        Properties props = new Properties();
-        props.put("bootstrap.servers", KAFKA_BROKER_IP + ":" + KAFKA_BROKER_PORT);
-        props.put("group.id", "iot");
-        props.put("enable.auto.commit", "true");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("key.deserializer", StringDeserializer.class.getName());
-        props.put("value.deserializer", StringDeserializer.class.getName());
-        return props;
-    }
+    private static final String MAX_IDLE_TIMEOUT = "86400000";
 
     public static Properties setupProducerProps() {
         Properties props = new Properties();
         props.put("bootstrap.servers", KAFKA_BROKER_IP + ":" + KAFKA_BROKER_PORT);
         props.put("acks", "all");
+        props.put("connections.max.idle.ms", MAX_IDLE_TIMEOUT);
         props.put("key.serializer", StringSerializer.class.getName());
         props.put("value.serializer", StringSerializer.class.getName());
         return props;
