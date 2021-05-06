dbc057d43faa8409e464e71e2b56fc2ce019ebbf
Nicolò Sonnino <swagpino@gmail.com>
2021-05-06 21:18:14 +0200

    finished javadocs

--- contact-tracing/kafka/src/main/java/it/polimi/middleware/kafka/producers/KafkaKSQLProducer.java
+++ contact-tracing/kafka/src/main/java/it/polimi/middleware/kafka/producers/KafkaKSQLProducer.java
@@ -14,7 +14,21 @@ import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 
-public class MainProducer {
+/**
+ * The KafkaSQLProducer class instantiates both a Kafka producer and a KSQL client (for Confluent Platform).
+ * <p>
+ * With the second one it creates a KSQL Stream for incoming connections from the MQTTSourceConnector writing to the
+ * mqtt-to-kafka-alerts topic; then this stream is materialized into a KSQL table (CONNECTION_TABLE) which prevents
+ * duplicates and lets the use of KSQL queries for retrieving results.
+ * <p>
+ * Each result is a client ID of a Contiki mote which established a connection with the mote firing the alert and
+ * it is used for every alert received in order to encapsulated it in a ProducerRecord and send it to the
+ * kafka-to-mqtt-alerts topic.
+ *
+ * @author Sonny
+ * @version 1.0
+ */
+public class KafkaKSQLProducer {
     private static final Logger logger = LoggerFactory.getLogger("Logger");
     private static final String connectionStream = "CREATE STREAM CONNECTIONS (" +
             "EventType VARCHAR , " +
@@ -38,13 +52,24 @@ public class MainProducer {
                 EMIT CHANGES;""";
     private static final String ksqlQuery = "SELECT * FROM ALERTS EMIT CHANGES;";
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments.
+     * @throws ExecutionException   the execution exception.
+     * @throws InterruptedException the interrupted exception.
+     */
     public static void main(String[] args) throws ExecutionException, InterruptedException {
+        //Kafka producer settings.
         Properties props = Utils.setupProducerProps();
         Producer<String, String> producer = new KafkaProducer<>(props);
+        //KSQL client creation.
         Client ksqlClient = Utils.createKSQLClient();
+        //List current KSQL streams on the broker/cluster.
         List<StreamInfo> streams = ksqlClient.listStreams().get();
         boolean alertStreamFound = false;
         boolean connectionStreamFound = false;
+
         for (StreamInfo stream : streams) {
             if (stream.getName().equals("ALERTS")) {
                 alertStreamFound = true;
@@ -61,6 +86,7 @@ public class MainProducer {
             ksqlClient.executeStatement(connectionStream).get();
         }
         boolean connectionTableFound = false;
+        //List current KSQL tables on the broker/cluster.
         List<TableInfo> tables = ksqlClient.listTables().get();
         for (TableInfo table : tables) {
             if (table.getName().equals("CONNECTION_TABLE")) {
@@ -68,28 +94,37 @@ public class MainProducer {
             }
         }
         if (!connectionTableFound) {
+            //Create Kafka Connection Table
             Map<String, Object> properties = Collections.singletonMap("auto.offset.reset", "earliest");
             ksqlClient.executeStatement(connectionTableQuery, properties).get();
         }
+        /*Stream query, particular KSQL query which runs continuously (the main benefit of using KSQLdb instead of a
+        regular DB).*/
         StreamedQueryResult streamedQueryResult = ksqlClient.streamQuery(ksqlQuery).get();
         while (true) {
+            //As soon as it receives a new row
             Row row = streamedQueryResult.poll(Duration.ofSeconds(40));
             if (row != null) {
+                //Get client ID
                 String clientId = row.values().getString(1);
+                //Create KSQL query for CONNECTION_TABLE
                 String pullQuery = "SELECT * FROM CONNECTION_TABLE WHERE CLIENTID='" + clientId + "';";
                 Map<String, Object> properties = new HashMap<>();
                 properties.put("auto.offset.reset", "latest");
+                //Property needed for KSQL v0.17 in order to unlock usage of non-key queries.
                 properties.put("ksql.query.pull.table.scan.enabled", true);
                 List<Row> queryResult1 = ksqlClient.executeQuery(pullQuery, properties).get();
                 logger.info("Received an alert from client " + clientId + ". Creating a mqtt alert message...");
+                //Each client ID ha a column in the table with a JsonArray storing all motes encountered.
                 for (Row result : queryResult1) {
                     JsonArray array = (JsonArray) result.getValue(2);
                     for (Object json : array) {
                         Timestamp timestamp = new Timestamp(System.currentTimeMillis());
                         String value = "{" +
-                                "\"AlertDest\":\"" + json +"\", " +
+                                "\"AlertDest\":\"" + json + "\", " +
                                 "\"AlertSource\":\"" + clientId + "\"" +
                                 "}";
+                        //Write on Kafka topic.
                         producer.send(new ProducerRecord<>("kafka-to-mqtt-alerts", timestamp.toString(), value));
                     }
                 }
--- contact-tracing/kafka/src/main/java/it/polimi/middleware/kafka/utils/Utils.java
+++ contact-tracing/kafka/src/main/java/it/polimi/middleware/kafka/utils/Utils.java
@@ -2,17 +2,24 @@ package it.polimi.middleware.kafka.utils;
 
 import io.confluent.ksql.api.client.Client;
 import io.confluent.ksql.api.client.ClientOptions;
-import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
+/**
+ * Utils class used for setup function (initially thought to be used in multiple classes).
+ */
 public class Utils {
     private static final String KAFKA_BROKER_IP = "51.103.25.211";
     private static final String KAFKA_BROKER_PORT = "9092";
     private static final int KAFKA_KSQL_PORT = 8088;
     private static final String MAX_IDLE_TIMEOUT = "86400000";
 
+    /**
+     * Sets producer props.
+     *
+     * @return the producer props.
+     */
     public static Properties setupProducerProps() {
         Properties props = new Properties();
         props.put("bootstrap.servers", KAFKA_BROKER_IP + ":" + KAFKA_BROKER_PORT);
@@ -22,7 +29,13 @@ public class Utils {
         props.put("value.serializer", StringSerializer.class.getName());
         return props;
     }
-    public static Client createKSQLClient(){
+
+    /**
+     * Create ksql client client with host and port defined.
+     *
+     * @return the client.
+     */
+    public static Client createKSQLClient() {
         ClientOptions options = ClientOptions.create()
                 .setHost(KAFKA_BROKER_IP)
                 .setPort(KAFKA_KSQL_PORT);
