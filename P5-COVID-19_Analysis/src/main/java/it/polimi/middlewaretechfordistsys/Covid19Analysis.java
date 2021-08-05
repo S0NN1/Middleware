46b55c71b255ac9d2cfae26b64517454f43efe3c
Luca Pirovano <luca.pirovano.98@gmail.com>
2021-08-05 13:10:01 +0200

    Feat: new version

--- P5-COVID-19_Analysis/src/main/java/it/polimi/middlewaretechfordistsys/Covid19Analysis.java
+++ P5-COVID-19_Analysis/src/main/java/it/polimi/middlewaretechfordistsys/Covid19Analysis.java
@@ -25,6 +25,7 @@ import static org.apache.spark.sql.functions.desc;
 public class Covid19Analysis {
 
     public static void main(String[] args) {
+        String filePath = args.length > 1 ? args[1] : "./";
         final SparkConf conf = new SparkConf().setAppName("Covid-19").setMaster(args.length > 0 ? args[0] : "local[4]");
         final JavaSparkContext sc = new JavaSparkContext(conf);
         SparkSession spark = SparkSession
@@ -35,27 +36,30 @@ public class Covid19Analysis {
         SQLContext sqlContext = new SQLContext(sc);
 
         // TASK 1
-        Dataset<Row> df = spark.read().format("csv").option("header", "true").option("inferSchema", true).load("resources/csv/ecdc/data.csv");
-        WindowSpec ws1 = Window.partitionBy("countriesAndTerritories").orderBy("date").rowsBetween(-6,0);
+        Dataset<Row> df = spark.read().format("csv").option("header", "true").option("inferSchema", true).load(filePath + "resources/csv/ecdc/data.csv");
+        WindowSpec ws1 = Window.partitionBy("countriesAndTerritories").orderBy("date", "geoId").rowsBetween(-6,0);
         Column col1 = functions.avg("cases").over(ws1);
         Dataset<Row> df1 = df.withColumn("date", functions.to_timestamp(df.col("dateRep"), "dd/MM/yyyy")).withColumn("movingAverage", functions.round(col1, 2));
+        df1.show(1000);
 
         // TASK 2
-        WindowSpec ws2 = Window.partitionBy("countriesAndTerritories").orderBy("date");
+        WindowSpec ws2 = Window.partitionBy("countriesAndTerritories").orderBy("date", "geoId");
         Dataset<Row> df2 = df1.withColumn("prevValue", functions.round(functions.lag("movingAverage", 1).over(ws2), 2));
         Column col3 = df2.col("movingAverage").minus(df2.col("prevValue"));
         Column col2 = col3.divide(df2.col("prevValue")).multiply(100).cast("float");
         Dataset<Row> df3 = df2.withColumn("perc_increase", functions.when(functions.isnull(col3),0)
                 .otherwise(functions.round(col2, 2))).drop("prevValue");
+        df3 = df3.filter(df3.col("perc_increase").isNotNull());
 
         //TASK 3
         WindowSpec ws3 = Window.partitionBy("date").orderBy(functions.desc("perc_increase"));
         Dataset<Row> df4 = df3.withColumn("rankingPercIncrease", dense_rank().over(ws3));
-        Dataset<Row> df5 = df4.where("rankingPercIncrease<=10").orderBy("date");
+        Dataset<Row> df5 = df4.where("rankingPercIncrease<=10").orderBy("date", "rankingPercIncrease");
 
 
         //Print results
-        df3.show(1000); // Query 1 and 2
+        df1.show(1000);
+        df3.show(1000); // Query 1 and 2*/
         df5.show(1000); // Query 3
 
     }
